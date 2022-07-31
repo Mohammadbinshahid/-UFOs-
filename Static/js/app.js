c06aae8d1426cc128bd651140a17927312b6bb7e
Mohammadbinshahid <Mohammadbinshahid@gmail.com>
2022-07-31 18:08:36 -0400

    created a table

--- Static/js/app.js
+++ Static/js/app.js
@@ -2,25 +2,22 @@
 const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
-function hello () {
-    console.log("Hello " + name);}
-// Simple JavaScript console.log statement
-function printHello();
-// Takes two numbers and adds them
-function addition(a, b) {
-    return a + b;
-  }
-  console.log(addition(4, 5));
- // Functions can call other functions
-function doubleAddition(c, d) {
-    var total = addition(c, d) * 2;
-    return total;
-  }
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
 
-  let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
-function listLoop(userList) {
-    for (var i = 0; i < userList.length; i++) {
-      console.log(userList[i]);
-    }
-console.log(friends)
-listLoop(friends);
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });
+}
